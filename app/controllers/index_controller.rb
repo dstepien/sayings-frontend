06dcdc75a3acde8f033c1d3a8ea5fc627089382c
dstepien <kontakt@dawidstepien.pl>
2015-02-26 16:49:11 +0100

    Parsing JSON response with content and author

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,6 +3,6 @@ require 'rest_client'
 class IndexController < ApplicationController
 
   def index
-    @saying = RestClient.get 'http://localhost:8080/say-1.0-SNAPSHOT/rest/say'.to_str
+    @saying = JSON.parse RestClient.get 'http://localhost:8080/say-1.0-SNAPSHOT/rest/say'.to_str
   end
 end
