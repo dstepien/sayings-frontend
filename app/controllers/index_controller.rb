7b255faa0995f2222b34f486c3ea01fd9e26a6fc
dstepien <kontakt@dawidstepien.pl>
2015-02-26 16:28:44 +0100

    Improve a HTML design

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,7 +3,6 @@ require 'rest_client'
 class IndexController < ApplicationController
 
   def index
-    response = RestClient.get 'http://localhost:8080/say-1.0-SNAPSHOT/rest/say'
-    @saying = response.to_str
+    @saying = RestClient.get 'http://localhost:8080/say-1.0-SNAPSHOT/rest/say'.to_str
   end
 end
